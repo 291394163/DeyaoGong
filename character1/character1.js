5e31d05548060ff98b1d732aedf7af1dc3ecfb81
DeyaoGong <291394163@qq.com>
2017-03-25 18:09:56 +0800

    update

---
+++ character1/character1.js
@@ -0,0 +1,502 @@
+/**
+ * Created by Administrator on 2017/3/15.
+ */
+var data = [
+    {
+        'question':'你最希望以下列哪种方式度过周末？',
+        'options':[
+            {
+                'url':'http://7xj5dd.com2.z0.glb.qiniucdn.com/1E_concert_.jpg',
+                'desc':'A.去演唱会找回青春',
+                'count':'h'
+            },{
+                'url':'http://7xj5dd.com2.z0.glb.qiniucdn.com/1E_gathering.jpg',
+                'desc':'B.和朋友海吃海喝',
+                'count':'h'
+            },{
+                'url':'http://7xj5dd.com2.z0.glb.qiniucdn.com/1I_gallery.jpg',
+                'desc':'C.去美术馆欣赏画作',
+                'count':'i'
+            },{
+                'url':'http://7xj5dd.com2.z0.glb.qiniucdn.com/1I_reading.jpg',
+                'desc':'D.在咖啡馆读书',
+                'count':'i'
+            }
+        ]
+    },{
+        'question':'你会选择下列哪一项活动作为爱好？',
+        'options':[
+            {
+                'url':'http://ac-waocd7vi.clouddn.com/dbef1b5ff954219f.JPG',
+                'desc':'A.跳街舞',
+                'count':'h'
+            },{
+                'url':'http://ac-waocd7vi.clouddn.com/09cc0b1db728a63e.jpg',
+                'desc':'B.K歌',
+                'count':'h'
+            },{
+                'url':'http://ac-waocd7vi.clouddn.com/20be2ea4eb7a9740.jpg',
+                'desc':'C.宅家看电影',
+                'count':'i'
+            },{
+                'url':'http://ac-waocd7vi.clouddn.com/9c8cc081287f5588.jpg',
+                'desc':'D.做瑜伽',
+                'count':'i'
+            }
+        ]
+    },{
+        'question':'你想在什么样的地方生活？',
+        'options':[
+            {
+                'url':'http://ac-waocd7vi.clouddn.com/a91f6a8824f78971.jpg',
+                'desc':'A.繁华都市',
+                'count':'h'
+            },{
+                'url':'http://ac-waocd7vi.clouddn.com/db4c157b192fb21b.jpg',
+                'desc':'B.海边城市',
+                'count':'h'
+            },{
+                'url':'http://ac-waocd7vi.clouddn.com/7501ce9a025c645f.jpg',
+                'desc':'C.僻静的树林',
+                'count':'i'
+            },{
+                'url':'http://ac-waocd7vi.clouddn.com/b92de6e4c0e9d9a3.jpg',
+                'desc':'D.安逸的小镇',
+                'count':'i'
+            }
+        ]
+    },{
+        'question':'选择一个适合你的专业？',
+        'options':[
+            {
+                'url':'http://ac-waocd7vi.clouddn.com/29e8865a623348e0.jpg',
+                'desc':'A.美术',
+                'count':'j'
+            },{
+                'url':'http://ac-waocd7vi.clouddn.com/4344ae567f75f23c.jpg',
+                'desc':'B.哲学',
+                'count':'j'
+            },{
+                'url':'http://ac-waocd7vi.clouddn.com/6f74c818a8f9e80a.jpg',
+                'desc':'C.工程',
+                'count':'k'
+            },{
+                'url':'http://ac-waocd7vi.clouddn.com/29c642c30d48774e.jpg',
+                'desc':'D.金融',
+                'count':'k'
+            }
+        ]
+    },{
+        'question':'选择一个感兴趣的职业？',
+        'options':[
+            {
+                'url':'http://ac-waocd7vi.clouddn.com/8f4c8558833cf077.jpg',
+                'desc':'A.厨师',
+                'count':'j'
+            },{
+                'url':'http://ac-waocd7vi.clouddn.com/31f01577d0dfbfb9.jpg',
+                'desc':'B.教师',
+                'count':'j'
+            },{
+                'url':'http://ac-waocd7vi.clouddn.com/5f0842eb1d1f0fbb.jpg',
+                'desc':'C.医生',
+                'count':'k'
+            },{
+                'url':'http://ac-waocd7vi.clouddn.com/f597de5923cef3b0.jpg',
+                'desc':'D.会计',
+                'count':'k'
+            }
+        ]
+    },{
+        'question':'以下几位名人你更想成为谁一般的人？',
+        'options':[
+            {
+                'url':'http://ac-waocd7vi.clouddn.com/9f8baed6cfd88ee9.jpg',
+                'desc':'A.知名演员范冰冰',
+                'count':'j'
+            },{
+                'url':'http://ac-waocd7vi.clouddn.com/0249326546070edf.jpg',
+                'desc':'B.著名导演陈凯歌',
+                'count':'j'
+            },{
+                'url':'http://ac-waocd7vi.clouddn.com/71f927026e37705d.jpg',
+                'desc':'C.美国总统奥巴马',
+                'count':'k'
+            },{
+                'url':'http://ac-waocd7vi.clouddn.com/05d913a59082a943.jpg',
+                'desc':'D.女总统季莫申科',
+                'count':'k'
+            }
+        ]
+    },{
+        'question':'以下几部电视剧，你对哪个最感兴趣？',
+        'options':[
+            {
+                'url':'http://ac-waocd7vi.clouddn.com/7e8a427de2860bde.jpg',
+                'desc':'A.欢乐颂',
+                'count':'l'
+            },{
+                'url':'http://ac-waocd7vi.clouddn.com/c44be8aec4c4643b.jpg',
+                'desc':'B.绯闻女孩',
+                'count':'l'
+            },{
+                'url':'http://ac-waocd7vi.clouddn.com/cf78f8c8cf0309bb.jpg',
+                'desc':'C.余罪',
+                'count':'m'
+            },{
+                'url':'http://ac-waocd7vi.clouddn.com/56db511f6d370df3.jpg',
+                'desc':'D.福尔摩斯',
+                'count':'m'
+            }
+        ]
+    },{
+        'question':'以下几本书，你更想买哪一本？',
+        'options':[
+            {
+                'url':'http://ac-waocd7vi.clouddn.com/563194d402ceb176.jpg',
+                'desc':'A.科幻类：哈利波特',
+                'count':'l'
+            },{
+                'url':'http://ac-waocd7vi.clouddn.com/1e18aa8e254bb726.jpg',
+                'desc':'B.爱情类：红楼梦',
+                'count':'l'
+            },{
+                'url':'http://ac-waocd7vi.clouddn.com/3d6d38b4a63e723d.jpg',
+                'desc':'C.职业类：影响力',
+                'count':'m'
+            },{
+                'url':'http://ac-waocd7vi.clouddn.com/fd475467b3f00858.jpg',
+                'desc':'D.应用类：孙子兵法',
+                'count':'m'
+            }
+        ]
+    },{
+        'question':'请从下列四个颜色中选出你最喜欢的？',
+        'options':[
+            {
+                'url':'http://ac-waocd7vi.clouddn.com/4c1593a043aed161.jpg',
+                'desc':'A.橙色',
+                'count':'l'
+            },{
+                'url':'http://ac-waocd7vi.clouddn.com/02422c0f0911976d.jpg',
+                'desc':'B.红色',
+                'count':'l'
+            },{
+                'url':'http://ac-waocd7vi.clouddn.com/f3c07a7d12c51059.jpg',
+                'desc':'C.蓝色',
+                'count':'m'
+            },{
+                'url':'http://ac-waocd7vi.clouddn.com/4f2ffafe62ec4e75.jpg',
+                'desc':'D.绿色',
+                'count':'m'
+            }
+        ]
+    },{
+        'question':'选出一个做你的手机背景？',
+        'options':[
+            {
+                'url':'http://ac-waocd7vi.clouddn.com/6b4bd54f5f768363.jpg',
+                'desc':'A.By:Doesburg',
+                'count':'n'
+            },{
+                'url':'http://ac-waocd7vi.clouddn.com/894e61b59f8cf109.jpg',
+                'desc':'B.By:Mondrian',
+                'count':'n'
+            },{
+                'url':'http://ac-waocd7vi.clouddn.com/8053dcdbeb678d04.jpg',
+                'desc':'C.By:Kardinsky',
+                'count':'o'
+            },{
+                'url':'http://ac-waocd7vi.clouddn.com/1da51d0f9e9a4a2a.jpg',
+                'desc':'D.By:Kusama',
+                'count':'o'
+            }
+        ]
+    },{
+        'question':'以下几个笔记，哪个更想你的？',
+        'options':[
+            {
+                'url':'http://ac-waocd7vi.clouddn.com/192bb7418ada7816.jpg',
+                'desc':'A.彩色+整洁',
+                'count':'n'
+            },{
+                'url':'http://ac-waocd7vi.clouddn.com/ca633dd0a70ead05.jpg',
+                'desc':'B.单色+整洁',
+                'count':'n'
+            },{
+                'url':'http://ac-waocd7vi.clouddn.com/a7a558752e65a5bc.jpg',
+                'desc':'C.单色+乱乱的',
+                'count':'o'
+            },{
+                'url':'http://ac-waocd7vi.clouddn.com/95334cabdc867aab.jpg',
+                'desc':'D.彩色+乱乱的',
+                'count':'o'
+            }
+        ]
+    },{
+        'question':'下列的装潢设计中，你最喜欢哪个？',
+        'options':[
+            {
+                'url':'http://ac-waocd7vi.clouddn.com/e552246db3ed27db.jpg',
+                'desc':'A.清新森林风',
+                'count':'n'
+            },{
+                'url':'http://ac-waocd7vi.clouddn.com/5323593cd6a7fe4e.jpg',
+                'desc':'B.日式极简风',
+                'count':'n'
+            },{
+                'url':'http://ac-waocd7vi.clouddn.com/42444bb38b25fcb9.jpeg',
+                'desc':'C.波西米亚风',
+                'count':'o'
+            },{
+                'url':'http://ac-waocd7vi.clouddn.com/222d14629783f1e5.jpg',
+                'desc':'D.欧式古典风',
+                'count':'o'
+            }
+        ]
+    }
+];
+var html1 = juicer($('#question-template').html().toString(),data);
+$('.right-box').html(html1);
+var html2 = juicer($('#options-template').html().toString(),data);
+$('.options-box').append(html2);
+
+
+var btn_next = $('.btn-next');
+var right_move = $('.right-move');
+var options_move = $('.options-move');
+var flex = $('.flex');
+var flex1 = $('.flex1');
+var option_item = $('.option-item');
+var btn_start = $('.btn-start');
+var btn_create = $('.btn-create');
+var back = $('.back');
+var page1 = $('.page1');
+var page2 = $('.page2');
+var page3 = $('.page3');
+var page4 = $('.page4');
+var answer = [];
+var that = this;
+
+var flag = true;
+var timer = null;
+
+that.index = 0;
+that.e = 0;
+that.f = 0;
+that.g = 0;
+that.h = 0;
+
+//初始化页面
+var option_item1 = $(options_move[0]).children('.option-item');
+// console.log(option_item1);
+lazyImg(option_item1);
+
+
+
+//点击开始
+btn_start.click(function(){
+    page1.addClass('hide1');
+    page2.removeClass('hide1');
+});
+
+
+
+
+
+//选择该题
+option_item.click(function(){
+    $(this).addClass('bg-change').siblings().removeClass('bg-change');
+    that.choose = $(this).attr('data-info');
+    // console.log(that.choose);
+    answer.push(that.choose);
+    // console.log(answer);
+    if(that.index == right_move.length-1){
+        btn_next.html('完成');
+    }
+    flag = false;
+});
+
+
+
+
+
+
+
+//点击下一题
+btn_next.click(function(){
+    // console.log(right_move);
+    // console.log(right_move[index]);
+    clearTimeout(timer);
+
+    if(!flag){
+        $(right_move[that.index]).addClass('hide4');
+        $(right_move[that.index+1]).removeClass('hide3');
+
+        $(options_move[that.index]).addClass('hide4');
+        $(options_move[that.index+1]).removeClass('hide3');
+
+        var option_item = $(options_move[that.index+1]).children('.option-item');
+        // console.log(option_item);
+        lazyImg(option_item);
+
+        $('.numerator').html(that.index+2);
+        that.index++;
+        if(that.index == right_move.length){
+            console.log(221);
+            page2.addClass('hide1');
+            page3.removeClass('hide1');
+        }
+    }
+
+
+    if(flag){
+        flex.addClass('show');
+
+        timer = setTimeout(function(){
+            flex.removeClass('show');
+        },1000);
+    }
+
+    flag = true;
+});
+//点击生成报告
+btn_create.click(function(){
+    var userName = $('input').val();
+    clearTimeout(timer);
+    if(!userName){
+        flex1.addClass('show');
+
+        timer = setTimeout(function(){
+            flex1.removeClass('show');
+        },1000);
+    }else{
+        $.each(answer,function(index,item){
+            if(item == 'h'){
+                that.e++;
+            }
+            if(item == 'j'){
+                that.f++;
+            }
+            if(item == 'l'){
+                that.g++;
+            }
+            if(item == 'n'){
+                that.h++;
+            }
+        });
+        var num1 = 'a';
+        var num2 = 'b';
+        var num3 = 'c';
+        var num4 = 'd';
+        if(that.e<2){
+            num1='w'
+        }else if(that.f<2){
+            num2 = 'x';
+        }else if(that.g<2){
+            num3 = 'y';
+        }else if(that.h<2){
+            num4 = 'z';
+        }
+        that.result = num1+num2+num3+num4;
+        console.log(that.result);
+        loadNumber(that.result);
+        console.log(that.number);
+
+        $.ajax({
+            type:'post',
+            url:'http://123.56.114.209:9191/common/sp/image',
+            data:{
+                'name':userName,
+                'number':that.number
+            },
+            success:function(responseData){
+                console.log(typeof responseData);
+                var _url = JSON.parse(responseData).data.url+'?imageView/2/w/500';
+                $('.result-img-url').attr('src',_url);
+                page3.addClass('hide1');
+                page4.removeClass('hide1');
+
+            },
+            error:function(){
+
+            }
+
+        })
+
+
+    }
+});
+//点击返回重做
+back.click(function(){
+    that.index = 0;
+
+    right_move.removeClass('hide4').addClass('hide3');
+    $(right_move[0]).removeClass('hide3');
+
+    options_move.removeClass('hide4').addClass('hide3');
+    $(options_move[0]).removeClass('hide3');
+
+    $('.numerator').html(that.index+1);
+
+    option_item.removeClass('bg-change');
+
+    btn_next.html('下一题');
+
+    page3.addClass('hide1');
+    page2.removeClass('hide1');
+});
+//转换数字函数
+function loadNumber(data){
+    that.number;
+    switch(data){
+        case 'abcd':that.number = '1';break;
+        case 'abcz':that.number = '2';break;
+        case 'abyd':that.number = '3';break;
+        case 'abyz':that.number = '4';break;
+        case 'axcd':that.number = '5';break;
+        case 'axcz':that.number = '6';break;
+        case 'axyd':that.number = '7';break;
+        case 'axyz':that.number = '8';break;
+        case 'wbcd':that.number = '9';break;
+        case 'wbcz':that.number = '10';break;
+        case 'wbyd':that.number = '11';break;
+        case 'wbyz':that.number = '12';break;
+        case 'wxcd':that.number = '13';break;
+        case 'wxcz':that.number = '14';break;
+        case 'wxyd':that.number = '15';break;
+        case 'wxyz':that.number = '16';break;
+    }
+}
+
+//图片懒加载函数
+function lazyImg(arg){
+    $.each(arg,function(index,item){
+        var target = $(item).children('.option-img');
+        var _url = target.attr('data-img');
+        target.css({'background-image':'url('+_url+'?imageView/2/w/200)'})
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
